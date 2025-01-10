25fbe2b43d01a5b1f37f42daad392084448ea878
tauhidul0821 <tuhin2093@gmail.com>
2025-01-11 02:57:46 +0600

    layout children modification

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,20 +6,26 @@ import Footer from '../components/Layout/Footer';
 const Dashboard = () => {
   return (
     <>
-      <div className="bg-gray-100 flex min-h-screen">
-        <Sidebar />
-        <div className="flex-1 flex flex-col">
-          <Header />
+      {/* <div className="bg-gray-100 flex min-h-screen"> */}
+        {/* <Sidebar /> */}
+        {/* <div className="flex-1 flex flex-col"> */}
+          {/* <Header /> */}
           <main className="flex-1 p-6">
-            <div className="bg-white rounded-lg shadow p-6">
+            {/* <div className="bg-white rounded-lg shadow p-6">
               <h1 className="text-2xl font-bold text-gray-700">Main Content Area</h1>
               <p className="mt-4 text-gray-600">This is where the dynamic content for each page will go.</p>
-            </div>
+            </div> */}
+
+              <h1 className="text-2xl font-bold text-gray-700">Main Content Area</h1>
+              <p className="mt-4 text-gray-600">This is where the dynamic content for each page will go.</p>
+
+
+
           </main>
           
-          <Footer />
-        </div>
-      </div>
+          {/* <Footer /> */}
+        {/* </div> */}
+      {/* </div> */}
     </>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+'use client';
 import Footer from "./components/Layout/Footer";
 import Sidebar from './components/Layout/Sidebar';
 import Header from './components/Layout/Header';
 import Dashboard from "./dashboard/page";
+import Profile from './dashboard/profile/page';
+import Students from './dashboard/students/page';
+
 
 export default function Home() {
   return (
     <>
-      <Dashboard /> 
+      <Dashboard />
       
     </>
   );
